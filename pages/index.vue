52099c88dab56a1b8c9ae836cfacea47e3090fb3
lawrenceagles <lawrenceagles@gmail.com>
2021-10-01 21:38:34 +0100

    added JobList component

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,75 @@
 <template>
-  <Tutorial/>
+  <div class="app">
+    <header>
+      <div class="title">
+        <h1>Remote Developer Jobs</h1>
+      </div>
+      <div class="order">
+        <button @click="handleClick('title')">Order by title</button>
+        <button @click="handleClick('salary')">Order by salary</button>
+        <button @click="handleClick('location')">Order by location</button>
+      </div>
+    </header> 
+
+    <JobList :jobs="jobs" :order="order" />
+  </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+  import { defineComponent, ref } from '@nuxtjs/composition-api';
+  import Job from '@/types/Jobs'
+  import OrderTerm from '@/types/OrderItems'
+  import JobList from '@/components/JobList.vue'
 
-export default Vue.extend({})
+  export default defineComponent({
+    name: 'App',
+    components: { JobList },
+    
+    setup() {
+      
+      const jobs = ref<Job[]>([
+        { title: 'React senior developer', location: 'New York', salary: 30000, id: '1' },
+        { title: 'Nodejs developer', location: 'London', salary: 40000, id: '2' },
+        { title: 'Flutter developer', location: 'Lagos', salary: 35000, id: '3' },
+        { title: 'Vuejs developer', location: 'Boston', salary: 21000, id: '4' },
+        { title: 'Svelte & TypeScript developer', location: 'San Francisco', salary: 32000, id: '5' }
+      ])
+
+      const order = ref<OrderTerm>('title')
+
+      const handleClick = (term: OrderTerm) => order.value = term
+
+      return { jobs, order, handleClick }
+    }
+  });
 </script>
+
+<style>
+  header {
+    text-align: center;
+  }
+  header .order {
+    margin-top: 20px;
+  }
+  button {
+    margin: 0 10px;
+    color: #1195c9;
+    border: 3px solid #1195c9;
+    background: #d5f0ff;
+    padding: 8px 16px;
+    border-radius: 4px;
+    cursor: pointer;
+    font-weight: bold;
+  }
+  header .title{
+    display: flex;
+    justify-content: center;
+  }
+  header img {
+    width: 60px;
+    margin-right: 20px;
+  }
+  header h1 {
+    font-size: 3em;
+  }
+</style>
